b215497d040019df658b713e36f6dc14be11b344
Ryoung27 <rryoung89@gmail.com>
2021-11-29 18:50:42 -0600

    lecture 1 first exercise peaks and valleys

---
+++ lecture1/straight_peak.py
@@ -0,0 +1,14 @@
+
+def printElement(arr, n):
+    for i in range(1, n - 1, 1):
+        if(arr[i] > arr[i-1] and arr[i] > arr[i +1]):
+            print(arr[i], end = " ")
+    if arr[-1] > arr[-2]:
+        print(arr[-1], end = " ")
+
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6,7,8,9,100,99, 100]
+    n = len(arr)
+    printElement(arr, len(arr))
