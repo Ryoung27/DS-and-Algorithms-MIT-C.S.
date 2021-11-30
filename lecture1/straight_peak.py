b85f944b32fd25168111868036c17841d8a88bb0
Ryoung27 <rryoung89@gmail.com>
2021-11-29 19:00:19 -0600

    updating based on problem definition

--- lecture1/straight_peak.py
+++ lecture1/straight_peak.py
@@ -1,9 +1,9 @@
 
 def printElement(arr, n):
     for i in range(1, n - 1, 1):
-        if(arr[i] > arr[i-1] and arr[i] > arr[i +1]):
+        if(arr[i] >= arr[i-1] and arr[i] >= arr[i +1]):
             print(arr[i], end = " ")
-    if arr[-1] > arr[-2]:
+    if arr[-1] >= arr[-2]:
         print(arr[-1], end = " ")
 
 
